1b20062125b2b1f7b95a040f6f2492b895de366f
kasim.badami <kasim.badami@rtcamp.com>
2012-10-19 17:02:34 +0530

    debug

--- controllers/ProjectTrackingGitoliteController.class.php
+++ controllers/ProjectTrackingGitoliteController.class.php
@@ -634,8 +634,7 @@
           $repository = $this->active_repository;
          
            $repo_details = ProjectGitolite::get_repo_details($repository->getId());
-           print_r($repo_details);
-           die();
+           
            if(is_array($repo_details) && count($repo_details) > 0)
            {
                
