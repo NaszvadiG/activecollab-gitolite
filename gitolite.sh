ca41936f9e50dc4c93ff815faaaba6aac502cac2
Mitesh Shah <Mitesh.Shah@rtCamp.com>
2012-10-26 20:00:17 +0530

    License Details Added

--- gitolite.sh
+++ gitolite.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# @VERSION 1.0
+# @AUTHOR  rtCamp Solutions Pvt. Ltd. (admin@rtcamp.com)
+#	   Mitesh Shah (mitesh.shah@rtcamp.com)
+
+# This script is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License at (http://www.gnu.org/licenses/) for
+# more details.
+
 
 
 # Variables
