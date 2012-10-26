c7fb365b8095e9ae882ceb03a1c8c8f8b597dce3
Mitesh Shah <Mitesh.Shah@rtCamp.com>
2012-10-26 17:19:22 +0530

    English Change On Messages

--- gitolite.sh
+++ gitolite.sh
@@ -13,7 +13,7 @@ if [ $Permission -ne 0 ]
 then
         echo
         echo -e "\033[31m Root Privilege Required... \e[0m" #| tee -ai $LOGFILE
-        echo -e "\033[31m Uses: sudo bash $0 [git-username] [php-username] \e[0m" #| tee -ai $LOGFILE
+        echo -e "\033[31m Usage: sudo bash $0 [git-username] [php-username] \e[0m" #| tee -ai $LOGFILE
         exit 100
 fi
 
@@ -29,7 +29,7 @@ OwnError()
 echo &>> $LOGFILE
 echo &>> $LOGFILE
 echo &>> $LOGFILE
-echo -e "\033[34m Gitolite Admin is setup started at `date` \e[0m" | tee -ai $LOGFILE
+echo -e "\033[34m Gitolite Admin Installation Started At `date` \e[0m" | tee -ai $LOGFILE
 # Detect Linux Distro
 
 # Detection Of Ubuntu
@@ -53,18 +53,18 @@ then
 	if [ $OPENSSH -ne 0 ] || [ $GITCORE -ne 0 ] || [ $CURL -ne 0 ]
 	then
 		# Update Cache
-		echo -e "\033[34m Updating apt cache... \e[0m" | tee -ai $LOGFILE
+		echo -e "\033[34m Updating APT Cache... \e[0m" | tee -ai $LOGFILE
 		sudo apt-get update || OwnError "Unable To Update APT Cache"
 
 		# Install Open SSH Server And Git
-		echo -e "\033[34m Installing openssh-server, git-core or curl... \e[0m"
+		echo -e "\033[34m Installing Open SSH Server, Git and Curl... \e[0m"
 		sudo apt-get -y install openssh-server git-core curl &>> $LOGFILE \
-		|| OwnError "Unable To Install Open SSH Server and Git"
+		|| OwnError "Unable To Install Open SSH Server, Git and Curl "
 	fi
 
 else
         echo | tee -ai $LOGFILE
-       	echo -e "\033[31m Currently this script support only ubuntu distro \e[0m"
+       	echo -e "\033[31m Currently This Script Supports Only Ubuntu Distro \e[0m"
        	exit 200
 fi
 
@@ -75,9 +75,9 @@ fi
 # Ask User If Script Run Withour ARGS
 if [ $# -lt 1 ]
 then
-	echo -e "\033[34m A user account will be created for gitolite setup... \e[0m" \
+	echo -e "\033[34m An User Account Will Be Created For Gitolite Admin Setup... \e[0m" \
 	| tee -ai $LOGFILE
-	read -p "Enter the username [git]: " GITUSER
+	read -p "Enter The Username [git]: " GITUSER
 
 # Enter Then Used Default Git
 if [[ $GITUSER = "" ]]
@@ -94,8 +94,8 @@ fi
 grep ^$GITUSER: /etc/passwd &>> $LOGFILE
 if [ $? -eq 0 ]
 then
-	echo -e "\033[31m The $GITUSER user is already exist !! \e[0m" | tee -ai $LOGFILE
-	echo -e "\033[31m Please select the different username !! \e[0m" | tee -ai $LOGFILE
+	echo -e "\033[31m The $GITUSER User Already Exists !! \e[0m" | tee -ai $LOGFILE
+	echo -e "\033[31m Please Select A Different Username !! \e[0m" | tee -ai $LOGFILE
         exit 102
 fi
 
@@ -103,25 +103,25 @@ fi
 echo -e "\033[34m Creating System User [$GITUSER]  \e[0m" | tee -ai $LOGFILE
 sudo adduser --system --home /home/$GITUSER --shell /bin/bash --group \
 --disabled-login --disabled-password --gecos 'git version control' $GITUSER &>> $LOGFILE \
-|| OwnError "Unable to create $GITUSER"
+|| OwnError "Unable To Create $GITUSER"
 
 # Copy Skeleton Contents
-echo -e "\033[34m Copying system files...  \e[0m" | tee -ai $LOGFILE
+echo -e "\033[34m Copying System Files...  \e[0m" | tee -ai $LOGFILE
 sudo -H -u $GITUSER cp /etc/skel/.profile /etc/skel/.bashrc /etc/skel/.bash_logout /home/$GITUSER/
 
 # Create a bin Directory For Git User
-echo -e "\033[34m Creating bin directory \e[0m" | tee -ai $LOGFILE
-sudo -H -u $GITUSER mkdir /home/$GITUSER/bin || OwnError "Unable to create bin directory"
+echo -e "\033[34m Creating bin Directory \e[0m" | tee -ai $LOGFILE
+sudo -H -u $GITUSER mkdir /home/$GITUSER/bin || OwnError "Unable To Create bin Directory"
 
 
 
 
 # Create a setup Directory For Gitolite Repository
-echo -e "\033[34m Creating setup directory \e[0m" | tee -ai $LOGFILE
+echo -e "\033[34m Creating setup Directory \e[0m" | tee -ai $LOGFILE
 sudo -H -u $GITUSER mkdir /home/$GITUSER/setup \
-|| OwnError "Unable to create setup directory"
+|| OwnError "Unable To Create setup Directory"
 
-cd /home/$GITUSER/setup || OwnError " Unable to change directory"
+cd /home/$GITUSER/setup || OwnError " Unable To Change Directory"
 
 echo | tee -ai $LOGFILE
 echo -e "\033[34m Cloning Gitolite Server Repository... \e[0m" | tee -ai $LOGFILE
@@ -129,9 +129,9 @@ sudo -H -u $GITUSER git clone git://github.com/sitaramc/gitolite  &>> $LOGFILE \
 || OwnError "Unable to clone gitolote repository"
 
 # Create a Symbolic Link For Gitolite in /home/git/bin Directory
-echo -e "\033[34m Creating Gitolite symbolic link  \e[0m" | tee -ai $LOGFILE
+echo -e "\033[34m Creating Gitolite Symbolic Link  \e[0m" | tee -ai $LOGFILE
 sudo -H -u $GITUSER gitolite/install -to /home/$GITUSER/bin \
-|| OwnError "Unable to create symbolic link for Gitolite"
+|| OwnError "Unable To Create Symbolic Link For Gitolite"
 
 
 
@@ -139,9 +139,9 @@ sudo -H -u $GITUSER gitolite/install -to /home/$GITUSER/bin \
 # Ask User If Script Run Withour ARGS
 if [ $# -lt 2 ]
 then
-	echo -e "\033[34m PHP username is given at Gitolite Admin [Need Help section] \e[0m" \
+	echo -e "\033[34m PHP Username Is Given At Gitolite Settings [Need Help Section] \e[0m" \
 	| tee -ai $LOGFILE
-	read -p " Enter the php username [www-data]:  " WEBUSER
+	read -p " Enter The PHP Username [www-data]:  " WEBUSER
 
 	if [[ $WEBUSER = "" ]]
 	then
@@ -155,7 +155,7 @@ fi
 
 
 # Add Web User to Git Group
-echo -e "\033[34m Adding $WEBUSER to $GITUSER group  \e[0m" | tee -ai $LOGFILE
+echo -e "\033[34m Adding $WEBUSER to $GITUSER Group  \e[0m" | tee -ai $LOGFILE
 sudo adduser $WEBUSER $GITUSER &>> $LOGFILE
 
 # Get The Web User Home Dir Path
@@ -164,53 +164,54 @@ echo WEBUSERHOME = $WEBUSERHOME &>> $LOGFILE
 if [ -z $WEBUSERHOME ]
 then
 	echo | tee -ai $LOGFILE
-	echo -e "\033[31m Unable to detect $WEBUSER home dir !! \e[0m" | tee -ai $LOGFILE
-	read -p "Enter the home dir path for $WEBUSER: " WEBUSERHOME
+	echo -e "\033[31m Unable To Detect $WEBUSER Home Directory !! \e[0m" | tee -ai $LOGFILE
+	read -p "Enter The Home Directory Path For [$WEBUSER]: " WEBUSERHOME
 fi
 
 # Checks .ssh Directory Exist
 ls $WEBUSERHOME/.ssh &>> tee -ai $LOGFILE
 if [ $? -ne 0 ]
 then
-	echo -e "\033[34m Creating .ssh directory \e[0m" | tee -ai $LOGFILE
-	sudo mkdir $WEBUSERHOME/.ssh || OwnError "Unable to crate $WEBUSERHOME/.ssh"
-	sudo chown -R $WEBUSER:$WEBUSER $WEBUSERHOME/.ssh || OwnError "Unable to chown .ssh"
+	echo -e "\033[34m Creating .ssh Directory \e[0m" | tee -ai $LOGFILE
+	sudo mkdir $WEBUSERHOME/.ssh || OwnError "Unable To Create $WEBUSERHOME/.ssh"
+	sudo chown -R $WEBUSER:$WEBUSER $WEBUSERHOME/.ssh \
+	|| OwnError "Unable To Change Ownership (chown) .ssh"
 fi
 
 # Checks Weather id_rsa Key Exist
 sudo ls  $WEBUSERHOME/.ssh/id_rsa &>> $LOGFILE
 if [ $? -eq 0 ]
 then
-	echo -e "\033[34m The ssh key id_rsa already exist \e[0m" | tee -ai $LOGFILE
+	echo -e "\033[34m The SSH Key id_rsa Already Exists \e[0m" | tee -ai $LOGFILE
 else
 
 	# Generate SSH Keys For Web User
-	echo -e "\033[34m Generating ssh keys for $WEBUSER \e[0m" | tee -ai $LOGFILE
+	echo -e "\033[34m Generating SSH Keys For $WEBUSER \e[0m" | tee -ai $LOGFILE
 	sudo -H -u $WEBUSER ssh-keygen -q -N '' -f $WEBUSERHOME/.ssh/id_rsa \
-	|| OwnError "Unable to create ssh keys for $WEBUSER"
+	|| OwnError "Unable To Create SSH Keys For $WEBUSER"
 fi
 
 
 # Create known_hosts file if not exist
 # Or if known_hosts exist update timestamp 
-sudo touch $WEBUSERHOME/.ssh/known_hosts || OwnError "Unable to create known_hosts"
+sudo touch $WEBUSERHOME/.ssh/known_hosts || OwnError "Unable To Create known_hosts"
 
 # Give 666 Permission To Add SSH Server Fingerprint
-sudo chmod 666 $WEBUSERHOME/.ssh/known_hosts || OwnError "Unable to chmod 666 known_hosts"
+sudo chmod 666 $WEBUSERHOME/.ssh/known_hosts || OwnError "Unable To chmod 666 known_hosts"
 
 # Use Wildcard For Match All The Domains
 sudo echo -n "* " >> $WEBUSERHOME/.ssh/known_hosts \
-|| OwnError "Unable to add wildcard as servername"
+|| OwnError "Unable To Add wildcard As Server-name"
 
 # Copy The SSH Server Fingerprint
 cat /etc/ssh/ssh_host_rsa_key.pub >> $WEBUSERHOME/.ssh/known_hosts \
-|| OwnError "Unable to add ssh server fingerprint"
+|| OwnError "Unable To Add SSH Server Fingerprint"
 
 
 # Give Back 644 Permission To Add SSH Server Fingerprint
-sudo chmod 644 $WEBUSERHOME/.ssh/known_hosts || OwnError "Unable to chmod 644 known_hosts"
+sudo chmod 644 $WEBUSERHOME/.ssh/known_hosts || OwnError "Unable To chmod 644 known_hosts"
 sudo chown $WEBUSER:$WEBUSER $WEBUSERHOME/.ssh/known_hosts \
-|| OwnError "Unable to chown known_hosts"
+|| OwnError "Unable To Chnage Ownership (chown) known_hosts"
 
 
 # Setup Gitolite Admin
@@ -218,27 +219,27 @@ echo | tee -ai $LOGFILE
 echo -e "\033[34m Setup Gitolite Admin...  \e[0m" | tee -ai $LOGFILE
 
 sudo cp $WEBUSERHOME/.ssh/id_rsa.pub /home/$GITUSER/$WEBUSER.pub \
-|| OwnError "Unable to copy $WEBUSER Pubkey"
+|| OwnError "Unable To Copy $WEBUSER Pubkey"
 	
 sudo chown $GITUSER:$GITUSER /home/$GITUSER/$WEBUSER.pub \
-|| OwnError "Unable to change ownership of $WEBUSER"
+|| OwnError "Unable To Change Ownership Of $WEBUSER"
 
 cd /home/$GITUSER
 
 sudo -H -u $GITUSER /home/$GITUSER/bin/gitolite setup -pk $WEBUSER.pub &>> $LOGFILE \
-|| OwnError "Unable to setup Gitolite Admin (Key)"
+|| OwnError "Unable To Setup Gitolite Admin (Key)"
 
 # Change UMASK Value
-echo -e "\033[34m Changing umask value  \e[0m" | tee -ai $LOGFILE
+echo -e "\033[34m Changing UMASK Value  \e[0m" | tee -ai $LOGFILE
 sudo -H -u $GITUSER sed -i 's/0077/0007/g' /home/$GITUSER/.gitolite.rc \
-|| OwnError "Unable to change UMASK"
+|| OwnError "Unable To Change UMASK"
 
 
 # Installing Post Receive Hooks
-echo -e "\033[34m Creating post-receive hooks \e[0m" | tee -ai $LOGFILE
+echo -e "\033[34m Creating post-receive Hooks \e[0m" | tee -ai $LOGFILE
 cd $BASEPATH
 
-cd ../../../public/ || OwnError "Unable to change directory for hookspath"
+cd ../../../public/ || OwnError "Unable To Change Directory For Hookspath"
 if [ -f .hookspath.rt ]
 then
 	HOOKSPATH=$(cat .hookspath.rt)
@@ -254,20 +255,20 @@ then
 	sudo -H -u $GITUSER /home/$GITUSER/bin/gitolite setup --hooks-only
 else
 	echo | tee -ai $LOGFILE
-	echo -e "\033[31m Can't create post-receive hooks...  \e[0m" | tee -ai $LOGFILE
+	echo -e "\033[31m Can't create post-receive Hooks !!  \e[0m" | tee -ai $LOGFILE
 	echo
 fi
 
 
 # Log Messages
 echo | tee -ai $LOGFILE
-echo -e "\033[34m For detailed installation messages use the following command \e[0m" \
+echo -e "\033[34m For Detailed Installation Messages Use The Following Command \e[0m" \
 | tee -ai $LOGFILE
 echo -e "\033[34m cat $LOGFILE \e[0m" | tee -ai $LOGFILE
 
 echo
-echo -e "\033[34m Gitolite Admin is successfully setup at `date` \e[0m" | tee -ai $LOGFILE
-echo -e "\033[34m Please go back to Gitolite Admin, test connection and save settings. \e[0m" \
+echo -e "\033[34m Gitolite Admin Is Successfully Installed On `date` \e[0m" | tee -ai $LOGFILE
+echo -e "\033[34m Please Go Back To Gitolite Admin, Test Connection And Save Settings. \e[0m" \
 | tee -ai $LOGFILE
 
 
