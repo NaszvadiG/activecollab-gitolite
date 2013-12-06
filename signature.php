5f46cb96f7257dfc94815a4d20eba0103748886c
Faishal <faishal.saiyed@rtcamp.com>
2013-12-06 15:49:23 +0530

    Fix for ac 4.1

--- signature.php
+++ signature.php
@@ -8,5 +8,7 @@
  */
 if ( defined ( "APPLICATION_VERSION" ) && version_compare ( APPLICATION_VERSION, "4.0", "<" ) )
     return 'TcFnJCm<jPNihLiGGzizrMUzh6:%6sEx79U2<#Vz';
-else
+else if( defined ( "APPLICATION_VERSION" ) && version_compare ( APPLICATION_VERSION, "4.1", "<" ) )
     return 'qw13XUlqFb2b^0R8)u<DH3nx*L6DY3>C06K7]D3M';
+else 
+	return '2D4xeWJpEI7p6uqf6nTNLHYweculoPe7133n8fH6';
