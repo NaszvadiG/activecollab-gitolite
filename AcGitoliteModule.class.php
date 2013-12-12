a87703011413aa4e9dca02269c72afae1bd6c97a
Faishal <faishal.saiyed@rtcamp.com>
2013-12-12 18:11:59 +0530

    V 1.3.9.1 released

--- AcGitoliteModule.class.php
+++ AcGitoliteModule.class.php
@@ -21,7 +21,7 @@ class AcGitoliteModule extends AngieModule {
      *
      * @var string
      */
-    protected $version = '1.3.9';
+    protected $version = '1.3.9.1';
 
     /**
      * Name of the project object class (or classes) that this module uses
