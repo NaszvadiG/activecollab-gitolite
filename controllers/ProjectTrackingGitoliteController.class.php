69464306a766db274fa632d56b9700442be02955
Faishal <faishal.saiyed@rtcamp.com>
2013-12-12 18:02:42 +0530

    Revert "Fix can add"
    This reverts commit db8c672eb2baebc6fdd9b5c33b52c3ad4865d910.

--- controllers/ProjectTrackingGitoliteController.class.php
+++ controllers/ProjectTrackingGitoliteController.class.php
@@ -70,8 +70,8 @@ class ProjectTrackingGitoliteController extends RepositoryController {
                     $permissions_array = array( );
                 }
 
-                if ( (array_key_exists ( $this->logged_user->getId (), $permissions_array ) && $permissions_array[ $this->logged_user->getId () ] > 1) || $this->logged_user->isAdministrator () || $this->logged_user->isProjectManager () || $this->active_project->isLeader ( $this->logged_user ) ) // || $repository->canAdd ( $this->logged_user ) {
-                {
+                if ( (array_key_exists ( $this->logged_user->getId (), $permissions_array ) && $permissions_array[ $this->logged_user->getId () ] > 1) || $this->logged_user->isAdministrator () || $this->logged_user->isProjectManager () || $this->active_project->isLeader ( $this->logged_user ) || $repository->canAdd ( $this->logged_user )
+                ) {
                     $allowed_repos[ ] = $repository->getId ();
                 }
                 /* $clone_url = $get_admin_settings['gitoliteuser']."@".$get_admin_settings['gitoliteserveradd'].":".$repository->getName();
